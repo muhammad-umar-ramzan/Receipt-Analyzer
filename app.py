c79c20382dcd820bdc5c21037e53c5cc370d852f
muhammad-umar-ramzan <ramzan87767@gmail.com>
2026-02-15 20:40:49 +0500

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ st.markdown("""
     }
     .success-box {
         padding: 20px;
-        background-color: #e8f4f8;
+        background-color: black;
         border-radius: 10px;
         border-left: 5px solid #4CAF50;
         margin: 10px 0;
